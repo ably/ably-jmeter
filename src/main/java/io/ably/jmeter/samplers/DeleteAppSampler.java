0b216aeeea7f0f78215cc52600e1eb963778fd37
Paddy Byers <paddy.byers@gmail.com>
2020-06-20 00:08:58 +0100

    Delete test app sampler: fix property access

--- src/main/java/io/ably/jmeter/samplers/DeleteAppSampler.java
+++ src/main/java/io/ably/jmeter/samplers/DeleteAppSampler.java
@@ -11,6 +11,8 @@ import org.apache.jmeter.threads.JMeterVariables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Properties;
+
 /**
  * A sampler that publishes a single message to a given channel using the Ably REST client
  */
@@ -28,27 +30,29 @@ public class DeleteAppSampler extends SetupSampler {
 		SampleResult result = new SampleResult();
 		result.setSampleLabel(getName());
 
-		JMeterVariables vars = JMeterContextService.getContext().getVariables();
-		AppSpec appSpec = (AppSpec) vars.getObject(BaseSampler.APP_SPEC);
-		if (appSpec == null) {
+		Properties properties = JMeterContextService.getContext().getProperties();
+		String appId = properties.getProperty(BaseSampler.APP_ID);
+		String apiKey = properties.getProperty(BaseSampler.API_KEY);
+		if (appId == null) {
 			logger.error("App not found");
 			return fillFailedResult(result, "App not found", 500);
 		}
 
+		JMeterVariables vars = JMeterContextService.getContext().getVariables();
 		String environment = vars.get(ENVIRONMENT);
 		if (environment == null) {
 			logger.error("Environment not found");
 			return fillFailedResult(result, "Environment not found", 500);
 		}
 
-		AblyRest ably = getAbly(environment, appSpec.keys[0].keyStr);
+		AblyRest ably = getAbly(environment, apiKey);
 		if (ably == null) {
 			logger.error("Unable to instance Ably library");
 			return fillFailedResult(result, "Unable to instance Ably library", 500);
 		}
 
 		try {
-			ably.http.request((Http.Execute<Void>) (http, callback) -> http.del("/apps/" + appSpec.appId, HttpUtils.defaultAcceptHeaders(false), null, null, true, callback)).sync();
+			ably.http.request((Http.Execute<Void>) (http, callback) -> http.del("/apps/" + appId, HttpUtils.defaultAcceptHeaders(false), null, null, true, callback)).sync();
 			return fillOKResult(result);
 		} catch (AblyException e) {
 			logger.error("Unable to create test app", e);
